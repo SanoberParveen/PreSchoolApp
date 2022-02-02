03294976b17535c125ba260448587957b0bd69ee
SanoberParveen <86728558+SanoberParveen@users.noreply.github.com>
2022-02-02 12:03:01 +0530

    Add files via upload

---
+++ lib/englishpage.dart
@@ -0,0 +1,346 @@
+import 'package:flutter/material.dart';
+import 'package:preschool/engmodule.dart';
+
+class EnglishPage extends StatefulWidget {
+  const EnglishPage({Key? key}) : super(key: key);
+
+  @override
+  _EnglishPageState createState() => _EnglishPageState();
+}
+
+class _EnglishPageState extends State<EnglishPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back),
+            tooltip: 'Go Back',
+          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        ),
+        body: ListView(
+          children: [
+            InkWell(
+              child: Container(
+                height: MediaQuery.of(context).size.height / 5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  color: Colors.yellow.shade100,
+                ),
+                margin: EdgeInsets.only(left: 65, right: 15, top: 25),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      //top: 25.0,
+                      left: -65,
+                      child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Image(
+                            image: AssetImage('assets/abc.png'),
+                            height: MediaQuery.of(context).size.height / 7,
+                          )),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                child: Text(
+                                  "Alphabet",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      // fontStyle: Fontstyl,
+                                      fontSize: 40,
+                                      fontStyle: FontStyle.normal,
+                                      fontWeight: FontWeight.bold
+                                      // fontFamily: 'chewy'
+                                      ),
+                                ),
+                                margin: EdgeInsets.only(left: 110, top: 40),
+                              ),
+                              // Image.asset(
+                              //   "ABC2.jpg",
+                              //   height: MediaQuery.of(context).size.height / 4,
+                              // ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                  // ignore: deprecated_member_use
+                  overflow: Overflow.visible,
+                  // clipBehavior: Clip.hardEdge,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AlphabetPage()),
+                );
+              },
+            ),
+            InkWell(
+              child: Container(
+                height: MediaQuery.of(context).size.height / 5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  color: Colors.brown.shade200,
+                ),
+                margin: EdgeInsets.only(left: 65, right: 15, top: 25),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      //top: 25.0,
+                      left: -65,
+                      child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Image(
+                            image: AssetImage('assets/num.jpg'),
+                            height: MediaQuery.of(context).size.height / 7,
+                          )),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                child: Text(
+                                  "Numbers",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      // fontStyle: Fontstyl,
+                                      fontSize: 40,
+                                      fontStyle: FontStyle.normal,
+                                      fontWeight: FontWeight.bold
+                                      // fontFamily: 'chewy'
+                                      ),
+                                ),
+                                margin: EdgeInsets.only(left: 115, top: 40),
+                              ),
+                              // Image.asset(
+                              //   "ABC2.jpg",
+                              //   height: MediaQuery.of(context).size.height / 4,
+                              // ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                  // ignore: deprecated_member_use
+                  overflow: Overflow.visible,
+                  // clipBehavior: Clip.hardEdge,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NumberPage()),
+                );
+              },
+            ),
+            InkWell(
+              child: Container(
+                height: MediaQuery.of(context).size.height / 5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  color: Colors.lime.shade200,
+                ),
+                margin: EdgeInsets.only(left: 65, right: 15, top: 25),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      //top: 25.0,
+                      left: -65,
+                      child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Image(
+                            image: AssetImage('assets/fruits.png'),
+                            height: MediaQuery.of(context).size.height / 5,
+                          )),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                child: Text(
+                                  "Fruits",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      // fontStyle: Fontstyl,
+                                      fontSize: 40,
+                                      fontStyle: FontStyle.normal,
+                                      fontWeight: FontWeight.bold
+                                      // fontFamily: 'chewy'
+                                      ),
+                                ),
+                                margin: EdgeInsets.only(left: 150, top: 40),
+                              ),
+                              // Image.asset(
+                              //   "ABC2.jpg",
+                              //   height: MediaQuery.of(context).size.height / 4,
+                              // ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                  // ignore: deprecated_member_use
+                  overflow: Overflow.visible,
+                  // clipBehavior: Clip.hardEdge,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FruitPage()),
+                );
+              },
+            ),
+            InkWell(
+              child: Container(
+                height: MediaQuery.of(context).size.height / 5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  color: Colors.green.shade300,
+                ),
+                margin: EdgeInsets.only(left: 65, right: 15, top: 25),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      //top: 25.0,
+                      left: -75,
+                      child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Image(
+                            image: AssetImage('assets/animals.png'),
+                            height: MediaQuery.of(context).size.height / 7,
+                          )),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                child: Text(
+                                  "Animals",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      // fontStyle: Fontstyl,
+                                      fontSize: 40,
+                                      fontStyle: FontStyle.normal,
+                                      fontWeight: FontWeight.bold
+                                      // fontFamily: 'chewy'
+                                      ),
+                                ),
+                                margin: EdgeInsets.only(left: 150, top: 40),
+                              ),
+                              // Image.asset(
+                              //   "ABC2.jpg",
+                              //   height: MediaQuery.of(context).size.height / 4,
+                              // ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                  // ignore: deprecated_member_use
+                  overflow: Overflow.visible,
+                  // clipBehavior: Clip.hardEdge,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AnimalPage()),
+                );
+              },
+            ),
+            InkWell(
+              child: Container(
+                height: MediaQuery.of(context).size.height / 5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  color: Colors.brown.shade200,
+                ),
+                margin: EdgeInsets.only(left: 65, right: 15, top: 25),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      //top: 25.0,
+                      left: -65,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(70),
+                          child: Image(
+                            image: AssetImage('assets/shapes.png'),
+                            width: MediaQuery.of(context).size.width / 3,
+                            height: MediaQuery.of(context).size.height / 5,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                child: Text(
+                                  "Shapes",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      // fontStyle: Fontstyl,
+                                      fontSize: 40,
+                                      fontStyle: FontStyle.normal,
+                                      fontWeight: FontWeight.bold
+                                      // fontFamily: 'chewy'
+                                      ),
+                                ),
+                                margin: EdgeInsets.only(left: 115, top: 40),
+                              ),
+                              // Image.asset(
+                              //   "ABC2.jpg",
+                              //   height: MediaQuery.of(context).size.height / 4,
+                              // ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                  // ignore: deprecated_member_use
+                  overflow: Overflow.visible,
+                  // clipBehavior: Clip.hardEdge,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ShapePage()),
+                );
+              },
+            ),
+            SizedBox(
+              height: 25,
+            )
+          ],
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+import 'package:preschool/marathipage.dart';
+import 'package:preschool/urdupage.dart';
+
+import 'englishpage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //  // title: 'Flutter Demo',
+      //   theme: ThemeData(
+
+      //     primarySwatch: Colors.blue,
+      //   ),
+      //color: Colors.yellowAccent.shade200,
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+
+      home: MyHomePage(
+        title: 'Lets Learn',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        title: Text(widget.title,
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'chewy',
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          InkWell(
+            child: Container(
+              height: MediaQuery.of(context).size.height / 5,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                color: Colors.pinkAccent.shade100,
+              ),
+              margin: EdgeInsets.only(left: 85, right: 15, top: 25),
+              child: Stack(
+                children: [
+                  Positioned(
+                    //top: 25.0,
+                    left: -85,
+                    child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Image(
+                          image: AssetImage('assets/ABC3.png'),
+                          height: MediaQuery.of(context).size.height / 7,
+                        )),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              child: Text(
+                                "English",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontFamily: 'chewy'),
+                              ),
+                              margin: EdgeInsets.only(left: 130, top: 40),
+                            ),
+                            // Image.asset(
+                            //   "ABC2.jpg",
+                            //   height: MediaQuery.of(context).size.height / 4,
+                            // ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+                // ignore: deprecated_member_use
+                overflow: Overflow.visible,
+                // clipBehavior: Clip.hardEdge,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => EnglishPage()),
+              );
+            },
+          ),
+          InkWell(
+            child: Container(
+              height: MediaQuery.of(context).size.height / 5,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                color: Colors.lime.shade200,
+              ),
+              margin: EdgeInsets.only(left: 85, right: 15, top: 25),
+              child: Stack(
+                children: [
+                  Positioned(
+                    //top: 25.0,
+                    left: -85,
+                    child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Image(
+                          image: AssetImage('assets/marathi.png'),
+                          height: MediaQuery.of(context).size.height / 4,
+                        )),
+                    // left: -5,
+                    // bottom: 120,
+                    // child: Padding(
+                    //   padding: const EdgeInsets.all(10),
+
+                    //   //   image: AssetImage("assetes/ABC2.jpg"),
+                    //   //   height: MediaQuery.of(context).size.height / 4,
+                    //   // ),
+                    //   child: Image(
+                    //     image: AssetImage("ABC2.jpg"),
+                    //     height: MediaQuery.of(context).size.height / 2,
+                    //   ),
+                    // )
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              child: Text(
+                                "Marathi",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontFamily: 'chewy'),
+                              ),
+                              margin: EdgeInsets.only(left: 100, top: 40),
+                            ),
+                            // Image.asset(
+                            //   "ABC2.jpg",
+                            //   height: MediaQuery.of(context).size.height / 4,
+                            // ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+                // ignore: deprecated_member_use
+                overflow: Overflow.visible,
+                // clipBehavior: Clip.hardEdge,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MarathiPage()),
+              );
+            },
+          ),
+          InkWell(
+            child: Container(
+              height: MediaQuery.of(context).size.height / 5,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                color: Colors.blue.shade200,
+              ),
+              margin: EdgeInsets.only(left: 100, right: 15, top: 25),
+              child: Stack(
+                children: [
+                  Positioned(
+                    //top: 25.0,
+                    left: -100,
+                    child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Image(
+                          image: AssetImage('assets/urdu.png'),
+                          height: MediaQuery.of(context).size.height / 4.5,
+                        )),
+                    // left: -5,
+                    // bottom: 120,
+                    // child: Padding(
+                    //   padding: const EdgeInsets.all(10),
+
+                    //   //   image: AssetImage("assetes/ABC2.jpg"),
+                    //   //   height: MediaQuery.of(context).size.height / 4,
+                    //   // ),
+                    //   child: Image(
+                    //     image: AssetImage("ABC2.jpg"),
+                    //     height: MediaQuery.of(context).size.height / 2,
+                    //   ),
+                    // )
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              child: Text(
+                                "Urdu",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontFamily: 'chewy'),
+                              ),
+                              margin: EdgeInsets.only(left: 100, top: 40),
+                            ),
+                            // Image.asset(
+                            //   "ABC2.jpg",
+                            //   height: MediaQuery.of(context).size.height / 4,
+                            // ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+                // ignore: deprecated_member_use
+                overflow: Overflow.visible,
+                // clipBehavior: Clip.hardEdge,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => UrduPage()),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
